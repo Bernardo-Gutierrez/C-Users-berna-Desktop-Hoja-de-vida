920c111d9cc36bc331fb861e2f350a46c7ce3f4b
Bernardo-Gutierrez <bernardoguterrezcoca@gmail.com>
2024-06-18 20:20:30 -0400

    Se agraga el Proyecto

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Hoja de vida cargada exitosamente');
+    // Puedes agregar más funcionalidades interactivas aquí si lo deseas.
+});
